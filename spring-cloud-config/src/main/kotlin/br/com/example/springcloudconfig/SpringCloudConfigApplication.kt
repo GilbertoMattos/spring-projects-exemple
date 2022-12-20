67a1abde85b088f3a7801054f5146d9fec1f54ea
Gilberto de Matos Costa <gilberto.costa@farmaciassaojoao.com.br>
2022-12-20 00:00:03 -0300

    commit inicial

---
+++ spring-cloud-config/src/main/kotlin/br/com/example/springcloudconfig/SpringCloudConfigApplication.kt
@@ -0,0 +1,13 @@
+package br.com.example.springcloudconfig
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.cloud.config.server.EnableConfigServer
+
+@SpringBootApplication
+@EnableConfigServer
+class SpringCloudConfigApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringCloudConfigApplication>(*args)
+}
